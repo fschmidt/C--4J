62b641c3e42d4358cc080d3f304ad10f0a4db121
Marcus Fröde <marcus.froede@gmx.de>
2012-06-14 20:23:50 +0200

    more comment

--- Container/map.h
+++ Container/map.h
@@ -11,6 +11,10 @@ class Map{
 public:
     typedef TreeIterator< Pair<K, V>, O > iterator;
 
+    /*
+     Saerch a value into the map if not found insert the value in the map
+     return A the value of the found Object if found the value else the new inserted note
+     */
     V& operator[](const K& key){
         iterator iterator = this->find(key);
         if(iterator != m_tree.end()){
@@ -22,38 +26,73 @@ public:
         }
     }
 
+    /*
+     insert a node in the map
+     return a TreeIteraor with the insertet node
+     */
     iterator insert(const Pair<K, V> &pair){
         return this->m_tree.insert(pair);
     }
 
+    /*
+     Saerch a value into the map
+     return A TreeNode Object if found the value else a null pointer
+     */
     iterator find(const K& key){
         return this->m_tree.find(key);
     }
 
+    /*
+     give return the root of the map
+     return root node
+     */
     iterator begin(){
         return this->m_tree.begin();
     }
 
+    /*
+     give return a iterator with a null pointer as note
+     return null node
+     */
     iterator end(){
         return this->m_tree.end();
     }
 
+    /*
+     find the lowest left node
+     return the lowest left node
+     */
     iterator first(){
         return this->m_tree.first();
     }
 
+    /*
+     find the lowest right node
+     return the lowest right node
+     */
     iterator last(){
         return this->m_tree.last();
     }
 
+    /*
+     find the lowest left node
+     return the key of the lowest left node
+     */
     const K& min(){
         return (this->first()->key());
     }
 
+    /*
+     find the lowest right node
+     return the key of the lowest right node
+     */
     const K& max(){
         return (this->last()->key());
     }
 
+    /*
+     clear the map
+     */
     void clear(){
         this->m_tree.clear();
     }
--- Container/tree.h
+++ Container/tree.h
@@ -23,35 +23,42 @@ public:
         return m_root;
     }
 
+    /*
+     insert a node in the tree
+     return a TreeIteraor with the insertet node
+     */
     TreeIterator<T,O>& insert(const T &value){
-        if(!m_root){
+        if(!m_root){ // no notes in the tree
             m_root = new TreeNode<T,O>(value);
             TreeIterator<T,O> *iterator = new TreeIterator<T,O>(m_root, this);
             return *iterator;
         }
 
         O lessThan;
-        if(lessThan(value, m_root->m_value)){
+        if(lessThan(value, m_root->m_value)){ //go in the left sube tree
             if((m_root->m_left) ){
                 Tree<T,O> *tree = new Tree<T,O>( m_root->m_left);
                 return tree->insert(value);
             }
+            // insert node
             TreeNode<T,O> *inserted = new TreeNode<T,O>(value, m_root);
             m_root->m_left = inserted;
             TreeIterator<T,O> *iterator = new TreeIterator<T,O>(m_root->m_left, this);
             return *iterator;
         }
-        if(lessThan(m_root->m_value, value)){
+        if(lessThan(m_root->m_value, value)){ //go in the right sube tree
             if((m_root->m_right)){
                 Tree<T,O> *tree = new Tree<T,O>( m_root->m_right);
                 return tree->insert(value);
             }
+            // insert node
             TreeNode<T,O> *inserted = new TreeNode<T,O>(value, m_root);
             m_root->m_right = inserted;
             TreeIterator<T,O> *iterator = new TreeIterator<T,O>(m_root, this);
             return *iterator;
         }
 
+        // replace current node
         TreeNode<T,O> *inserted = new TreeNode<T,O>(value, m_root);
         if(lessThan(value, m_root->m_value)){
             m_root->m_left = inserted;
@@ -63,6 +70,9 @@ public:
 
     }
 
+    /*
+     clear the tree
+     */
     void clear(){
         if(m_root->m_left){
             Tree leftTree = m_root->m_left;
@@ -77,22 +87,42 @@ public:
         m_root = 0;
     }
 
+    /*
+     give return the root of the tree
+     return root node
+     */
     TreeIterator<T,O> begin(){
         return TreeIterator<T,O>(m_root, this);
     }
 
+    /*
+     give return a iterator with a null pointer as note
+     return null node
+     */
     TreeIterator<T,O> end(){
         return TreeIterator<T,O>(0, this);
     }
 
+    /*
+     find the lowest left node
+     return the lowest left node
+     */
     TreeIterator<T,O> first(){
         return TreeIterator<T,O>(m_root->findFirst(), this);
     }
 
+    /*
+     find the lowest right node
+     return the lowest right node
+     */
     TreeIterator<T,O> last(){
         return TreeIterator<T,O>(m_root->findLast(), this);
     }
 
+    /*
+     Saerch a value into the tree
+     return A TreeNode Object if found the value else a null pointer
+     */
     TreeIterator<T,O> find(const T &value){
         TreeNode<T,O> *node = this->m_root->find(value);
         if(node != 0){
